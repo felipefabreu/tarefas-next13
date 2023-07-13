a5fa84fad3eaa08239831fc20aefa0c86c0f7ab1
Felipe <felipefabreu@gmail.com>
2023-07-12 22:54:36 -0300

    primeiro commit com layout ja quebrado

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-        Home
+        Dashboard
     </h2>
   )
 }
