4924a639635fdd85b5e743773090c20160b0b655
Felipe <felipefabreu@gmail.com>
2023-07-13 11:21:45 -0300

    ajustando components e layout padrao

---
+++ src/app/components/header.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Header() {
+  return (
+    <header className='bg-neutral-800'>
+        <nav className="left-0  mx-auto flex p-6 lg:px-8" aria-label="Global">
+          <div className="flex lg:flex-1">
+            <a href="/" className="-m-1.5 p-1.5 h-8 w-auto">
+              <span className="text-2xl font-bold leading-7 ">_tarefas</span>
+            </a>
+          </div>
+        </nav>
+      </header>
+  )
+}
---
+++ src/app/components/index.ts
@@ -0,0 +1,4 @@
+import Header from "./header";
+import Aside from "./aside";
+
+export {Header, Aside}
