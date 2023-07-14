286e437c4bb34c478bc79c3f0faec88d0c4aee8e
Felipe <felipefabreu@gmail.com>
2023-07-13 23:03:56 -0300

    adicionando register page

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image'
+import { SignInForm } from '../components'
 
 export default function Home() {
   return (
-    <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-        SignUp
-    </h2>
+    <>
+      <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+          Register
+      </h2>
+
+      <SignInForm/>
+
+    </>
   
   )
 }
